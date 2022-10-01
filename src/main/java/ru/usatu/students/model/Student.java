2f7257fbdac7dabdfb02f5ae5b92ea520532bdb4
Lenovo <rozhechka@gmail.com>
2022-10-01 12:14:08 +0500

    варрвыывоо

--- src/main/java/ru/usatu/students/model/Student.java
+++ src/main/java/ru/usatu/students/model/Student.java
@@ -1,7 +1,15 @@
 package ru.usatu.students.model;
 
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+import java.util.Objects;
+
+@XmlAccessorType(XmlAccessType.FIELD)
 public class Student {
+    @XmlElement(name="id")
     private int id;
+    @XmlElement(name="name")
     private String name;
 
     public Student(){
@@ -26,4 +34,17 @@ public class Student {
     public void setName(String name) {
         this.name = name;
     }
+    @Override
+    public boolean equals(Object o){
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Student student = (Student) o;
+        return id == student.id;
+    }
+
+    @Override
+    public int hashCode() {return Objects.hash(id);}
+
+
+
 }
---
+++ src/main/java/ru/usatu/students/service/StudentServiceXML.java
@@ -0,0 +1,85 @@
+package ru.usatu.students.service;
+
+
+import org.springframework.stereotype.Service;
+import ru.usatu.students.model.Student;
+import ru.usatu.students.model.StudentList;
+
+
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.Marshaller;
+import javax.xml.bind.Unmarshaller;
+import java.io.File;
+import java.util.List;
+
+@Service
+public class StudentServiceXML implements StudentService {
+    private static final String FILE_PATH = "F:\\projects\\students2\\src\\main\\resources\\students.xml";
+
+    @Override
+    public List<Student> getStudents() throws Exception {
+        return getStudentList().getStudents();
+
+    }
+@Override
+    public Student getStudent(int id) throws Exception{
+        return getStudents().stream().filter(student -> student.getId()==id)
+                .findFirst().orElse(new Student());
+
+}
+
+    public Student editStudent(int id, String name) {
+        return null;
+    }
+
+    public void deleteStudent(int id) {
+
+    }
+
+    @Override
+    public Student addStudent(Student student) throws Exception {
+        StudentList studentList = getStudentList();
+        studentList.getStudents().add(student);
+        save(studentList);
+        return student;
+}
+
+   @Override
+    public Student editStudent(int id,String name) throws Exception{
+        Studentlist studentList = getStudentList();
+        Student findStudent = studentList.getStudents().stream().filter(student -> student.getId() == id)
+                .findFirst().orElse(null);
+        if (findStudent == null){
+            return new Student();
+        }
+        findStudent.setName(name);
+        save(studentList);
+        return findStudent;
+    }
+
+    @Override
+    public void deleteStudent(int id) throws Exception{
+        StudentList studentList = getStudentList();
+        Student findStudent = studentList.getStudents().stream().filter(student -> student.getId() == id)
+                .findFirst().orElse(null);
+        if (findStudent != null){
+            studentList.getStudents().remove(findStudent);
+            save(studentList);
+        }
+    }
+    private StudentList getStudentList() throws Exception{
+        File file = new File(FILE_PATH);
+        JAXBContext context = JAXBContext.newInstance(StudentList.class);
+        Unmarshaller unmarshaller = context.createUnmarshaller();
+        return (StudentList) unmarshaller.unmarshal(file);
+
+    }
+    private void save(StudentList studentList) throws Exception{
+        File file = new File(FILE_PATH);
+        JAXBContext context = JAXBContext.newInstance(StudentList.class);
+        Marshaller marshaller = context.createMarshaller();
+        marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, Boolean.TRUE);
+        marshaller.marshal(studentList, file);
+
+    }
+}
