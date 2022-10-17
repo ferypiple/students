44b808fca4797472680d9cc1405ee7858f5ad900
arsla <arslan321asyanov@gmail.com>
2022-10-17 14:37:33 +0500

    delete controller and some changes

--- src/main/java/ru/usatu/students/controller/StudentController.java
+++ src/main/java/ru/usatu/students/controller/StudentController.java
@@ -4,16 +4,16 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import ru.usatu.students.model.Student;
 import ru.usatu.students.service.StudentService;
-import ru.usatu.students.service.StudentServiceCollection;
+import ru.usatu.students.service.StudentServiceXML;
 
 import java.util.List;
 
 @RestController
 @RequestMapping("/students")
 public class StudentController {
-    private StudentServiceCollection studentService;
+    private StudentServiceXML studentService;
 
-    public StudentController(StudentServiceCollection studentService) {
+    public StudentController(StudentServiceXML studentService) {
         this.studentService= studentService;
     }
     @GetMapping
@@ -22,17 +22,17 @@ public class StudentController {
         return studentService.getStudents(); }
     @GetMapping("/{id}")
     public @ResponseBody
-    Student getStudent(@PathVariable int id) {return studentService.getStudent(id);}
+    Student getStudent(@PathVariable int id) throws Exception {return studentService.getStudent(id);}
 
     @PostMapping
     public @ResponseBody
-    Student addStudent(@RequestBody Student student) {return studentService.addStudent(student);}
+    Student addStudent(@RequestBody Student student) throws Exception {return studentService.addStudent(student);}
 
     @PutMapping("/{id}")
     public @ResponseBody
-    Student editStudent(@PathVariable int id, @RequestBody String name) {return studentService.editStudent(id,name);}
+    Student editStudent(@PathVariable int id, @RequestBody String name) throws Exception {return studentService.editStudent(id,name);}
 
     @DeleteMapping("/{id}")
     @ResponseStatus(HttpStatus.OK)
-    public void deleteStudent(@PathVariable int id) { studentService.deleteStudent(id);}
+    public void deleteStudent(@PathVariable int id) throws Exception { studentService.deleteStudent(id);}
 }
